1a63781222b8d25befac923560d9b6cd2838d51b
lt610 <1421681158@qq.com>
2020-07-31 17:30:28 +0800

    7.31 upload

--- hello.py
+++
@@ -1,6 +0,0 @@
-import argparse
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--name', type=str)
-    args = parser.parse_args()
-    print('hello {}'.format(args.name))
